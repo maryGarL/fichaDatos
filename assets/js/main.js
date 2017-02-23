f743c2c034835ff8ea89bd9df84ce38699d4379a
Mariel Garcia <maryel_565@hotmail.com>
2017-02-23 17:50:47 -0500

    ficha datos

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+var nombre= document.getElementById('name');
+var apellido=document.getElementById('surname');
+var dni=document.getElementById('identifier');
+var direccion=document.getElementById('address');
+
+var resultados= document.getElementById('resultados');
+var boton= document.getElementById('botoncito');
+boton.addEventListener('click',function datos(event){
+  event.preventDefault();
+  var general= document.getElementsByClassName('lleno');
+  if(general[0].value.length==0||general[1].value.length==0||general[2].value.length==0||general[3].value.length==0){
+    window.alert('Falta completar');
+  } else {
+    resultados.innerHTML+="<ul>"+
+      "<li> Nombre: "+ nombre.value+"</li>"+
+      "<li> Apellido: " + apellido.value+"</li>"+
+      "<li> DNI: " + dni.value+"</li>"+
+      "<li> Direccion: " + direccion.value+"</li>"+
+    "</ul>"
+  };
+});
